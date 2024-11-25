833fead1ab30320e8491d7cb3b6045e4205c1bbb
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2024-11-25 05:30:54 +0530

    [feat]: basic intro page

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
-use std::io::{self, Read};
-
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+#![warn(clippy::all, clippy::pedantic)]
+mod editor;
+use editor::Editor;
+mod terminal;
 
 fn main() {
-    enable_raw_mode().unwrap();
-    for b in io::stdin().bytes(){
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-                println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-            } else {
-                println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-            }
-            if c == 'q' {
-                disable_raw_mode().unwrap();
-                break;
-            }
-    }
+    Editor::default().run();
 }
